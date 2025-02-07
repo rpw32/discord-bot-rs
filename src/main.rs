4b7be1d99a0477a794729671ba0cab0788901623
Riley Wagner <rpw.32@live.com>
2025-02-06 21:58:58 -0600

    implemented play functionality upon specific input message trigger

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::sync::Arc;
 
 use dotenv::dotenv;
 
@@ -12,31 +13,157 @@ use serenity::all::ChannelId;
 use serenity::all::User;
 use songbird::SerenityInit;
 
+// Event related imports to detect track creation failures.
+use songbird::events::{Event, EventContext, EventHandler as VoiceEventHandler, TrackEvent};
+
+use tokio::time::Duration;
+
 struct Data {} // User data, which is stored and accessible in all command invocations
 type Error = Box<dyn std::error::Error + Send + Sync>;
 type Context<'a> = poise::Context<'a, Data, Error>;
 
-fn _get_user_voice_channel<'a>(i_user: &'a User, i_guild: &'a GuildRef) -> Option<&'a ChannelId> {
+fn get_user_voice_channel<'a>(i_user: &'a User, i_guild: GuildRef<'a>) -> Option<ChannelId> {
     i_guild
         .voice_states
         .get(&i_user.id)
-        .and_then(|voice_state| voice_state.channel_id.as_ref())
+        .and_then(|voice_state| voice_state.channel_id)
 }
 
-// async fn join(songbird_manager: Option<Arc<Songbird>>, guild_id: GuildId, channel_id: ChannelId) {
-//     let manager = songbird_manager
-//         .expect("Songbird Voice client placed in at initialisation.")
-//         .clone();
+async fn _join(ctx: &serenity::Context, guild_id: &serenity::GuildId, channel_id: &ChannelId) {
+    let manager = songbird::get(&ctx)
+        .await
+        .expect("Songbird Voice client placed in at initialisation.")
+        .clone();
 
-//     if let Ok(handler_lock) = manager.join(guild_id, channel_id).await {
-//         // Attach an event handler to see notifications of all track errors.
-//         let mut handler = handler_lock.lock().await;
-//         handler.add_global_event(TrackEvent::Error.into(), TrackErrorNotifier);
-//     }
-// }
+    if let Ok(handler_lock) = manager.join(*guild_id, *channel_id).await {
+
+        // Print message to log that the bot successfully joined the channel
+        let _ = channel_id.say(&ctx.http, "Joined the voice channel!").await;
+
+        // Attach an event handler to see notifications of all track errors.
+        let mut handler = handler_lock.lock().await;
+        handler.add_global_event(TrackEvent::Error.into(), TrackErrorNotifier);
+
+        // After joining, leave after a timeout
+        leave_after_timeout(ctx, guild_id, handler.clone()).await;
+    }
+}
+
+async fn play(ctx: &serenity::Context, guild_id: &serenity::GuildId, channel_id: &ChannelId) -> Result<(), Error> {
+
+    let manager = songbird::get(&ctx)
+        .await
+        .expect("Songbird Voice client placed in at initialisation.")
+        .clone();
+
+    if let Ok(handler_lock) = manager.join(*guild_id, *channel_id).await {
+
+        // Print message to log that the bot successfully joined the channel
+        channel_id.say(&ctx.http, "Joined the voice channel!").await.unwrap();
+
+        // Attach an event handler to see notifications of all track errors.
+        let mut handler = handler_lock.lock().await;
+        handler.add_global_event(TrackEvent::Error.into(), TrackErrorNotifier);
+        handler.add_global_event(TrackEvent::Play.into(), TrackStartNotifier);
+
+        // Load an audio file (change the path to your audio file)
+        let track = songbird::input::File::new("./caroline.mp3").into();
+
+        // Play the audio
+        handler.play(track);
+
+        // Let the user know that the bot is playing the audio
+        channel_id.say(&ctx.http, "Now playing your requested audio!").await.unwrap();
+
+        // After joining, leave after a timeout
+        leave_after_timeout(ctx, guild_id, handler.clone()).await;
+    }
+    
+    Ok(())
+}
+
+async fn leave_after_timeout(ctx: &serenity::Context, guild_id: &serenity::GuildId, mut handler: songbird::Call) {
+    // Define the timeout duration
+    let timeout_duration = Duration::from_secs(3); // 10 seconds timeout
+    tokio::time::sleep(timeout_duration).await;
+
+    // Leave the voice channel after the timeout period
+    if let Err(e) = handler.leave().await {
+        println!("Failed to leave the voice channel: {}", e);
+    } else {
+        println!("Bot left the voice channel.");
+    }
+}
+
+struct TrackErrorNotifier;
+
+#[serenity::async_trait]
+impl VoiceEventHandler for TrackErrorNotifier {
+    async fn act(&self, ctx: &EventContext<'_>) -> Option<Event> {
+        if let EventContext::Track(track_list) = ctx {
+            for (state, handle) in *track_list {
+                println!(
+                    "Track {:?} encountered an error: {:?}",
+                    handle.uuid(),
+                    state.playing
+                );
+            }
+        }
+
+        None
+    }
+}
+
+struct TrackStartNotifier;
+
+#[serenity::async_trait]
+impl VoiceEventHandler for TrackStartNotifier {
+    async fn act(&self, ctx: &EventContext<'_>) -> Option<Event> {
+        println!("Track has started playing!");
+        if let EventContext::Track(track_list) = ctx {
+            for (state, handle) in *track_list {
+                println!(
+                    "Track {:?} encountered an error: {:?}",
+                    handle.uuid(),
+                    state.playing
+                );
+            }
+        }
+
+        None
+    }
+}
+
+async fn event_handler(
+    ctx: &serenity::Context,
+    event: &serenity::FullEvent,
+    _framework: poise::FrameworkContext<'_, Data, Error>,
+    data: &Data,
+) -> Result<(), Error> {
+    match event {
+        serenity::FullEvent::Ready { data_about_bot, .. } => {
+            println!("Logged in as {}", data_about_bot.user.name);
+        }
+        serenity::FullEvent::Message { new_message } => {
+            if new_message.content.to_lowercase().contains("sweet caroline")
+                && new_message.author.id != ctx.cache.current_user().id
+            {
+                println!("Message receive!");
+                if let Some(channel_id) = get_user_voice_channel(&new_message.author, new_message.guild(&ctx.cache).unwrap()) {
+                    play(ctx, &new_message.guild_id.unwrap(), &channel_id).await.unwrap();
+                }
+                else {
+                    println!("User {} was not in a voice channel! Unable to join.", &new_message.author.name);
+                }
+            }
+        }
+        _ => {}
+    }
+    Ok(())
+}
 
 /// Displays your or another user's account creation date
-#[poise::command(prefix_command)]
+#[poise::command(slash_command, prefix_command)]
 async fn ping(ctx: Context<'_>) -> Result<(), Error> {
     ctx.channel_id().say(&ctx.http(), "Pong!").await?;
 
@@ -54,11 +181,14 @@ async fn main() {
     let validation_result = validate_token(&token);
     if validation_result.is_ok() {
         // Set gateway intents, which decides what events the bot will be notified about
-        let intents = GatewayIntents::non_privileged();
+        let intents = GatewayIntents::non_privileged() | serenity::GatewayIntents::MESSAGE_CONTENT;
 
         let framework = poise::Framework::builder()
             .options(poise::FrameworkOptions {
                 commands: vec![ping()],
+                event_handler: |ctx, event, framework, data| {
+                    Box::pin(event_handler(ctx, event, framework, data))
+                },
                 ..Default::default()
             })
             .setup(|ctx, _ready, framework| {
